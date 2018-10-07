6e8bff858bab7ac1ae8a31dc913a035c2217e598
Илья Котов <zwaren@yandex.ru>
2018-10-07 20:16:33 +0300

    Добавьте файлы проекта.

---
+++ Kotov_PIbd-21_C-sharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kotov_PIbd_21_C_sharp
+{
+	static class Program
+	{
+		/// <summary>
+		/// Главная точка входа для приложения.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
