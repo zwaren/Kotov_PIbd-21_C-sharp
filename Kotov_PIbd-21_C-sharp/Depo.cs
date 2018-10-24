d56c375d1bcb154dca9e8f0a2386050fecea5d45
Илья Котов <zwaren@yandex.ru>
2018-10-24 17:27:41 +0400

    lab3 completed

---
+++ Kotov_PIbd-21_C-sharp/Depo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotov_PIbd_21_C_sharp
+{
+	class Depo<T> where T : class, ITransport
+	{
+		private T[] _places;
+		private int PictureWidth { get; set; }
+		private int PictureHeight { get; set; }
+		private int _placeSizeWidth = 210;
+		private int _placeSizeHeight = 80;
+
+		public Depo(int sizes, int pictureWidth, int pictureHeight)
+		{
+			_places = new T[sizes];
+			PictureWidth = pictureWidth;
+			PictureHeight = pictureHeight;
+			for (int i = 0; i < _places.Length; i++)
+			{
+				_places[i] = null;
+			}
+		}
+
+		public static int operator +(Depo<T> p, T loco)
+		{
+			for (int i = 0; i < p._places.Length; i++)
+			{
+				if (p.CheckFreePlace(i))
+				{
+					p._places[i] = loco;
+					p._places[i].SetPosition(5 + i / 5 * p._placeSizeWidth + 5,
+					i % 5 * p._placeSizeHeight + 15, p.PictureWidth, p.PictureHeight);
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public static T operator -(Depo<T> p, int index)
+		{
+			if (index < 0 || index > p._places.Length)
+			{
+				return null;
+			}
+			if (!p.CheckFreePlace(index))
+			{
+				T loco = p._places[index];
+				p._places[index] = null;
+				return loco;
+			}
+			return null;
+		}
+
+		private bool CheckFreePlace(int index)
+		{
+			return _places[index] == null;
+		}
+
+		public void Draw(Graphics g)
+		{
+			DrawMarking(g);
+			for (int i = 0; i < _places.Length; i++)
+			{
+				if (!CheckFreePlace(i))
+				{
+					_places[i].DrawTransport(g);
+				}
+			}
+		}
+		private void DrawMarking(Graphics g)
+		{
+			Pen pen = new Pen(Color.Black, 3);
+			g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
+			for (int i = 0; i < _places.Length / 5; i++)
+			{
+				for (int j = 0; j < 6; ++j)
+				{
+					g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight,
+					i * _placeSizeWidth + 110, j * _placeSizeHeight);
+				}
+				g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, 400);
+			}
+		}
+	}
+}
---
+++ Kotov_PIbd-21_C-sharp/FormDepo.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Kotov_PIbd_21_C_sharp
+{
+	partial class FormDepo
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBoxDepo = new System.Windows.Forms.PictureBox();
+			this.buttonParkLoco = new System.Windows.Forms.Button();
+			this.buttonParkSteeamLoco = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.pictureBoxLocomotive = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.maskedTextBoxPlace = new System.Windows.Forms.MaskedTextBox();
+			this.buttonTake = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepo)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxLocomotive)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBoxDepo
+			// 
+			this.pictureBoxDepo.Location = new System.Drawing.Point(13, 13);
+			this.pictureBoxDepo.Name = "pictureBoxDepo";
+			this.pictureBoxDepo.Size = new System.Drawing.Size(622, 386);
+			this.pictureBoxDepo.TabIndex = 0;
+			this.pictureBoxDepo.TabStop = false;
+			// 
+			// buttonParkLoco
+			// 
+			this.buttonParkLoco.Location = new System.Drawing.Point(641, 13);
+			this.buttonParkLoco.Name = "buttonParkLoco";
+			this.buttonParkLoco.Size = new System.Drawing.Size(131, 36);
+			this.buttonParkLoco.TabIndex = 1;
+			this.buttonParkLoco.Text = "Припарковать локомотив";
+			this.buttonParkLoco.UseVisualStyleBackColor = true;
+			this.buttonParkLoco.Click += new System.EventHandler(this.buttonParkLoco_Click);
+			// 
+			// buttonParkSteeamLoco
+			// 
+			this.buttonParkSteeamLoco.Location = new System.Drawing.Point(641, 55);
+			this.buttonParkSteeamLoco.Name = "buttonParkSteeamLoco";
+			this.buttonParkSteeamLoco.Size = new System.Drawing.Size(131, 37);
+			this.buttonParkSteeamLoco.TabIndex = 2;
+			this.buttonParkSteeamLoco.Text = "Припарковать паровой локомотив";
+			this.buttonParkSteeamLoco.UseVisualStyleBackColor = true;
+			this.buttonParkSteeamLoco.Click += new System.EventHandler(this.buttonParkSteeamLoco_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.buttonTake);
+			this.groupBox1.Controls.Add(this.maskedTextBoxPlace);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.pictureBoxLocomotive);
+			this.groupBox1.Location = new System.Drawing.Point(641, 230);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(131, 169);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Забрать локомотив";
+			// 
+			// pictureBoxLocomotive
+			// 
+			this.pictureBoxLocomotive.Location = new System.Drawing.Point(7, 101);
+			this.pictureBoxLocomotive.Name = "pictureBoxLocomotive";
+			this.pictureBoxLocomotive.Size = new System.Drawing.Size(118, 62);
+			this.pictureBoxLocomotive.TabIndex = 0;
+			this.pictureBoxLocomotive.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(7, 32);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(42, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Место:";
+			// 
+			// maskedTextBoxPlace
+			// 
+			this.maskedTextBoxPlace.Location = new System.Drawing.Point(55, 29);
+			this.maskedTextBoxPlace.Name = "maskedTextBoxPlace";
+			this.maskedTextBoxPlace.Size = new System.Drawing.Size(70, 20);
+			this.maskedTextBoxPlace.TabIndex = 5;
+			// 
+			// buttonTake
+			// 
+			this.buttonTake.Location = new System.Drawing.Point(7, 58);
+			this.buttonTake.Name = "buttonTake";
+			this.buttonTake.Size = new System.Drawing.Size(118, 23);
+			this.buttonTake.TabIndex = 6;
+			this.buttonTake.Text = "Забрать";
+			this.buttonTake.UseVisualStyleBackColor = true;
+			this.buttonTake.Click += new System.EventHandler(this.buttonTake_Click);
+			// 
+			// FormDepo
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 411);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.buttonParkSteeamLoco);
+			this.Controls.Add(this.buttonParkLoco);
+			this.Controls.Add(this.pictureBoxDepo);
+			this.Name = "FormDepo";
+			this.Text = "Депо";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxDepo)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxLocomotive)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBoxDepo;
+		private System.Windows.Forms.Button buttonParkLoco;
+		private System.Windows.Forms.Button buttonParkSteeamLoco;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.PictureBox pictureBoxLocomotive;
+		private System.Windows.Forms.Button buttonTake;
+		private System.Windows.Forms.MaskedTextBox maskedTextBoxPlace;
+		private System.Windows.Forms.Label label1;
+	}
+}
