a41141420a09800e902d97b0bdaee564c2fdd71c
Илья Котов <zwaren@yandex.ru>
2018-11-10 00:26:27 +0400

    lab4 completed

--- Kotov_PIbd-21_C-sharp/Depo.cs
+++ Kotov_PIbd-21_C-sharp/Depo.cs
@@ -9,7 +9,8 @@ namespace Kotov_PIbd_21_C_sharp
 {
 	class Depo<T> where T : class, ITransport
 	{
-		private T[] _places;
+		private Dictionary<int, T> _places;
+		private int _maxCount;
 		private int PictureWidth { get; set; }
 		private int PictureHeight { get; set; }
 		private int _placeSizeWidth = 210;
@@ -17,22 +18,23 @@ namespace Kotov_PIbd_21_C_sharp
 
 		public Depo(int sizes, int pictureWidth, int pictureHeight)
 		{
-			_places = new T[sizes];
+			_maxCount = sizes;
+			_places = new Dictionary<int, T>(sizes);
 			PictureWidth = pictureWidth;
 			PictureHeight = pictureHeight;
-			for (int i = 0; i < _places.Length; i++)
-			{
-				_places[i] = null;
-			}
 		}
 
 		public static int operator +(Depo<T> p, T loco)
 		{
-			for (int i = 0; i < p._places.Length; i++)
+			if (p._places.Count == p._maxCount)
+			{
+				return -1;
+			}
+			for (int i = 0; i < p._maxCount; i++)
 			{
 				if (p.CheckFreePlace(i))
 				{
-					p._places[i] = loco;
+					p._places.Add(i, loco);
 					p._places[i].SetPosition(5 + i / 5 * p._placeSizeWidth + 5,
 					i % 5 * p._placeSizeHeight + 15, p.PictureWidth, p.PictureHeight);
 					return i;
@@ -43,14 +45,10 @@ namespace Kotov_PIbd_21_C_sharp
 
 		public static T operator -(Depo<T> p, int index)
 		{
-			if (index < 0 || index > p._places.Length)
-			{
-				return null;
-			}
 			if (!p.CheckFreePlace(index))
 			{
 				T loco = p._places[index];
-				p._places[index] = null;
+				p._places.Remove(index);
 				return loco;
 			}
 			return null;
@@ -58,25 +56,24 @@ namespace Kotov_PIbd_21_C_sharp
 
 		private bool CheckFreePlace(int index)
 		{
-			return _places[index] == null;
+			return !_places.ContainsKey(index);
 		}
 
 		public void Draw(Graphics g)
 		{
 			DrawMarking(g);
-			for (int i = 0; i < _places.Length; i++)
+			var keys = _places.Keys.ToList();
+			for (int i = 0; i < keys.Count; i++)
 			{
-				if (!CheckFreePlace(i))
-				{
-					_places[i].DrawTransport(g);
-				}
+				_places[i].DrawTransport(g);
 			}
 		}
+
 		private void DrawMarking(Graphics g)
 		{
 			Pen pen = new Pen(Color.Black, 3);
-			g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
-			for (int i = 0; i < _places.Length / 5; i++)
+			g.DrawRectangle(pen, 0, 0, (_maxCount / 5) * _placeSizeWidth, 480);
+			for (int i = 0; i < _maxCount / 5; i++)
 			{
 				for (int j = 0; j < 6; ++j)
 				{
@@ -84,6 +81,19 @@ namespace Kotov_PIbd_21_C_sharp
 					i * _placeSizeWidth + 110, j * _placeSizeHeight);
 				}
 				g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, 400);
+			}
+		}
+
+		public T this[int index]
+		{
+			get
+			{
+				if (_places.ContainsKey(index))
+				{
+					return _places[index];
+				}
+				return null;
+
 			}
 		}
 	}
